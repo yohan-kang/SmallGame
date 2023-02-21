f5933b4499c8cdde877098fade1c768aab60c014
dygks5048 <rmvmf123@labsd.net>
2023-02-21 13:03:41 +0900

    기초적인  예외 처리,콜백 버그 수정

--- FindNumber/FindNumber.py
+++ FindNumber/FindNumber.py
@@ -12,32 +12,23 @@ def intro() :
 
 
 
+# 유저가 작은 숫자 큰숫자 적게 하기 그중에 있게하기 
+
 def findNumber(min,max,num,bool,lst):
     while bool != True:
-
-
         x = input("{} Is this your number? : ".format(num))
         
         if x.upper() == "LOW":
-            # max = (num-1 // 2)
             max = num-1
-            # a = lst[:max]
-
             int_lst = lst[min-1:max]
-            # print("alst:{}".format(a))
-            # print("min:{},max:{}".format(min,max))
-            print("a:{}".format(int_lst))
+            print("list:{}".format(int_lst))
             num = (min + max) // 2
 
         elif x.upper() == "UP":
-            # min = (num+1 // 2)
             min = num+1 
-            
+    
             int_lst = lst[min-1:max]
-            # b = lst[min:]
-            # print("blst:{}".format(b))
-            # print("min:{},max:{}".format(min,max))
-            print("a:{}".format(int_lst))
+            print("list:{}".format(int_lst))
             num = (min + max) // 2
 
         elif x.upper() == "YES":
@@ -47,77 +38,82 @@ def findNumber(min,max,num,bool,lst):
             bool = True
         else :
             print("Invalid input Please write 'UP' or 'Low'.")
-            findNumber(min,max,num,bool)
+            bool = False
+            # findNumber(min,max,num,bool,lst)
 
             
-        if len(int_lst) == 2:
-            bool = caseOf2(int_lst)
+        if len(lst[min-1:max]) == 2:
+            bool = caseOf2(lst[min-1:max])
             break
-        elif len(int_lst) == 3:
-            bool = caseOf3(int_lst)
+        elif len(lst[min-1:max]) == 3:
+            bool = caseOf3(lst[min-1:max])
             break
 
 
 # 어쩌면 2개남은 상황에서 왼쪽에것이 아니면 자르고 하나남은 배열에서 그 배열에 관한 함수를 만들면 통일되서 좋을것 같다 
 
 def caseOf1(lst) :
-    x = input("{} Is this your number? : ".format(lst))
-    if x.upper() == "LOW" or x.upper() == "UP":
-        print("거짓말쟁이 이 숫자는 마지막 숫자야 그래서 당신이 찾는 숫자는 없어")
-        return True
-    elif x.upper() == "YES":
-        print("S~~~~~o Easy")
-        return True
-    elif x.upper() == "EXIT":
-        return True
-    else :
-        print("Invalid input Please write 'UP' or 'Low'.\n")
-        caseOf1(lst)
+    bool = False
+    while bool != True:
+        x = input("{} Is this your number? : ".format(lst))
+        if x.upper() == "LOW" or x.upper() == "UP":
+            print("거짓말쟁이 이 숫자는 마지막 숫자야 그래서 당신이 찾는 숫자는 없어")
+            return True
+        elif x.upper() == "YES":
+            print("S~~~~~o Easy")
+            return True
+        elif x.upper() == "EXIT":
+            return True
+        else :
+            print("Invalid input Please write 'UP' or 'Low'.\n")
+            bool = False
+        # caseOf1(lst)
 
 
 def caseOf2(lst) :
+    bool = False
+    while bool != True:
+        x = input("{} Is this your number? : ".format(lst[0]))
 
-    x = input("{} Is this your number? : ".format(lst[0]))
+        if x.upper() == "LOW":
+            print("거짓말쟁이 이 숫자보다 작은 숫자는 없어 ")
+            return True
+        elif x.upper() == "UP":
+            bool = caseOf1(lst[1])
+            return bool
 
-    if x.upper() == "LOW":
-        print("거짓말쟁이 이 숫자보다 작은 숫자는 없어 ")
-    elif x.upper() == "UP":
-        bool = caseOf1(lst[1])
-        return bool
-
-    elif x.upper() == "YES":
-        print("S~~~~~o Easy")
-        return True
-    elif x.upper() == "EXIT":
-        return True
-    else :
-        print("Invalid input Please write 'UP' or 'Low'.\n")
-        caseOf2(lst)
+        elif x.upper() == "YES":
+            print("S~~~~~o Easy")
+            return True
+        elif x.upper() == "EXIT":
+            return True
+        else :
+            print("Invalid input Please write 'UP' or 'Low'.\n")
+            bool = False
+        # caseOf2(lst)
 
 def caseOf3(lst) :
-    x = input("{} Is this your number? : ".format(lst[1]))
+    bool = False
+    while bool != True:
+        x = input("{} Is this your number? : ".format(lst[1]))
 
-    if x.upper() == "LOW":
-        bool=caseOf1(lst[0])
-        return bool
+        if x.upper() == "LOW":
+            bool=caseOf1(lst[0])
+            return bool
 
+        elif x.upper() == "UP":
+            bool=caseOf1(lst[2])
+            return bool
 
-
-    elif x.upper() == "UP":
-        bool=caseOf1(lst[2])
-        return bool
-
-
-
-
-    elif x.upper() == "YES":
-        print("S~~~~~o Easy")
-        return True
-    elif x.upper() == "EXIT":
-        return True
-    else :
-        print("Invalid input Please write 'UP' or 'Low'.\n")
-        caseOf3(lst)
+        elif x.upper() == "YES":
+            print("S~~~~~o Easy")
+            return True
+        elif x.upper() == "EXIT":
+            return True
+        else :
+            print("Invalid input Please write 'UP' or 'Low'.\n")
+            bool = False
+            # caseOf3(lst)
 
     
 
@@ -141,7 +137,7 @@ def reGame() :
         reGame()
 
 
-max = 50
+max = 100
 min = 1
 lst=list(range(min,max+1))
 bool = False
